8f332ffd2b8dcccdeeb4fb98305f8121e06a7cc0
RutledgeDixon <rutledgedixon@letu.edu>
2025-04-02 19:34:49 -0500

    Added functionality

--- RoomResPlusPlus/RoomResPlusPlus/Form2.Designer.cs
+++ RoomResPlusPlus/RoomResPlusPlus/Form2.Designer.cs
@@ -31,10 +31,10 @@
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.RoomResTitle = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.password = new System.Windows.Forms.TextBox();
+            this.signIn = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,12 +42,12 @@
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.password, 0, 4);
             this.tableLayoutPanel1.Controls.Add(this.label2, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.RoomResTitle, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.email, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.signIn, 0, 5);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 50);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -83,15 +83,15 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "School Email:                   ";
             // 
-            // textBox1
+            // email
             // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(248, 219);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(325, 34);
-            this.textBox1.TabIndex = 4;
-            this.textBox1.Text = "myname@letu.edu";
+            this.email.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.email.BackColor = System.Drawing.SystemColors.Window;
+            this.email.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.email.Location = new System.Drawing.Point(248, 219);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(325, 34);
+            this.email.TabIndex = 4;
             // 
             // label2
             // 
@@ -103,34 +103,36 @@
             this.label2.TabIndex = 5;
             this.label2.Text = "Password:                   ";
             // 
-            // textBox2
+            // password
             // 
-            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBox2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(248, 317);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(325, 34);
-            this.textBox2.TabIndex = 6;
+            this.password.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.password.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(248, 317);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(325, 34);
+            this.password.TabIndex = 6;
             // 
-            // button1
+            // signIn
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.AutoSize = true;
-            this.button1.BackColor = System.Drawing.Color.DarkBlue;
-            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.Location = new System.Drawing.Point(330, 383);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 55);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Sign in";
-            this.button1.UseVisualStyleBackColor = false;
+            this.signIn.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.signIn.AutoSize = true;
+            this.signIn.BackColor = System.Drawing.Color.DarkBlue;
+            this.signIn.Font = new System.Drawing.Font("MS Reference Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signIn.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.signIn.Location = new System.Drawing.Point(330, 383);
+            this.signIn.Name = "signIn";
+            this.signIn.Size = new System.Drawing.Size(161, 55);
+            this.signIn.TabIndex = 7;
+            this.signIn.Text = "Sign in";
+            this.signIn.UseVisualStyleBackColor = false;
+            this.signIn.Click += new System.EventHandler(this.Button1_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(822, 513);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -149,10 +151,10 @@
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Label RoomResTitle;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox password;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.Button signIn;
     }
 }
--- RoomResPlusPlus/RoomResPlusPlus/Form2.cs
+++ RoomResPlusPlus/RoomResPlusPlus/Form2.cs
@@ -12,9 +12,33 @@ namespace RoomResPlusPlus
 {
     public partial class Form2 : Form
     {
+
+        List<(string Username, string Password)> userCredentials = new List<(string, string)>
+            {
+                ("user1", "password1"),
+                ("user2", "password2"),
+                ("user3", "password3")
+            };
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string user = email.Text;
+            string pass = password.Text;
+
+            bool isAuthenticated = userCredentials.Any(cred => cred.Username == user && cred.Password == pass);
+
+            if (isAuthenticated)
+            {
+                MessageBox.Show("Login Successful");
+            }
+            else
+            {
+                MessageBox.Show("Login Failed");
+            }
         }
     }
 }
